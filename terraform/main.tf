5fe36396a22b2da02bd7d877d16d1847fd6249ac
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-12-04 22:24:11 -0300

    Fix:ajuste

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c55b159cbfafe1f0" 
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "terraform-example"
