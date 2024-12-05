5229009d20286cf178ca75784451ee9d02884432
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-12-04 22:37:26 -0300

    Ajustando o main.tf para pegar a chave na pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,28 +2,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Gerar a chave privada e pública com Terraform
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Criar o par de chaves na AWS
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+# Criar a instância EC2
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" 
+  ami           = "ami-0c55b159cbfafe1f0" # AMI padrão
   instance_type = "t2.nano"
+  key_name      = aws_key_pair.deployer.key_name  # Chave pública para acesso SSH
 
   tags = {
     Name = "terraform-example"
   }
 
+  # Provisionamento remoto via SSH para instalar Docker
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y docker.io",
+      "sudo usermod -aG docker ubuntu",  # Adicionar o usuário ubuntu ao grupo docker
       "sudo systemctl start docker",
       "sudo systemctl enable docker"
     ]
-  }
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/id_rsa")
-    host        = self.public_ip
+    # Conexão SSH para a instância EC2
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # Usuário padrão do Ubuntu
+      private_key = tls_private_key.example.private_key_pem
+      host        = self.public_ip
+    }
   }
 }
 
