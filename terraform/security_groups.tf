14db140a2a481516aa025b84556dc567b58e103b
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-12-06 23:29:40 -0300

    ajustando o arquivo que puxa

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "allow_ssh17" {
-  name        = "allow_ssh17"
+resource "aws_security_group" "allow_ssh20" {
+  name        = "allow_ssh20"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-0859a6c0d7f723e53" # vpc id da sua conta
 
