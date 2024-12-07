50f820ed371ad0ed4ae353b8d7e851b74401e637
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-12-06 23:44:17 -0300

    removendo os duplicados

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,16 @@ provider "aws" {
 resource "aws_iam_role" "ec2_role" {
   name               = "ec2_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      },
-    ]
-  })
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:CreateRole",
+      "Resource": "*"
+    }
+  ]
+}
+)
 }
 
 resource "aws_s3_bucket" "my_bucket" {
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "allow_ssh20" {
-  name        = "allow_ssh20"
+resource "aws_security_group" "allow_ssh22" {
+  name        = "allow_ssh22"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-0859a6c0d7f723e53" # vpc id da sua conta
 
