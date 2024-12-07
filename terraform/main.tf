77ca2bf31d46c92a826b3933ab6a5836c80ea4de
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-12-07 00:00:49 -0300

    ajustando caminho

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,19 @@ provider "aws" {
 resource "aws_iam_role" "ec2_role" {
   name               = "ec2_role"
   assume_role_policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "iam:CreateRole",
-      "Resource": "*"
-    }
-  ]
+    "Version" = "2012-10-17",
+    "Statement" = [
+      {
+        "Effect"    = "Allow"
+        "Action"    = "sts:AssumeRole"
+        "Principal" = {
+          "Service" = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
-)
-}
+
 resource "random_string" "suffix" {
   length  = 8
   special = false
@@ -27,47 +29,37 @@ resource "aws_s3_bucket" "my_bucket" {
   acl    = "private"
 }
 
-
 resource "aws_s3_bucket_object" "requirements_txt" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "requirements.txt"
-  source = "path_to_local_file/requirements.txt"  # Caminho para o arquivo local
+  source = "./requirements.txt"  
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "app_py" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "app.py"
-  source = "path_to_local_file/app.py"  # Caminho para o arquivo local
+  source = "./app.py"  
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "dockerfile" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "Dockerfile"
-  source = "path_to_local_file/Dockerfile"  # Caminho para o arquivo local
+  source = "./Dockerfile"  
   acl    = "private"
 }
 
-# Gerar a chave privada e pública com Terraform
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-# Criar o par de chaves na AWS
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key${random_string.suffix.result}"
   public_key = tls_private_key.example.public_key_openssh
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
-# Criar Security Group para permitir acesso SSH
 resource "aws_security_group" "allow_ssh" {
   name_prefix = "allow_ssh_"
   ingress {
@@ -84,7 +76,6 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-# Criar Instância EC2
 resource "aws_instance" "ec2_instance" {
   ami                    = "ami-0ac80df6eff0e70b5"
   instance_type          = "t2.nano"
@@ -95,12 +86,12 @@ resource "aws_instance" "ec2_instance" {
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
-              apt-get install -y awscli
+              apt-get install -y awscli python3-pip
+              pip3 install -r /home/ubuntu/app/requirements.txt
               EOF
 
   provisioner "remote-exec" {
     inline = [
-      "aws s3 cp s3://${aws_s3_bucket.my_bucket.bucket}/requirements.txt /home/ubuntu/app/requirements.txt",
       "aws s3 cp s3://${aws_s3_bucket.my_bucket.bucket}/app.py /home/ubuntu/app/app.py",
       "aws s3 cp s3://${aws_s3_bucket.my_bucket.bucket}/Dockerfile /home/ubuntu/app/Dockerfile"
     ]
